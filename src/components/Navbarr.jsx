26e83db5f2585d5d389544cc057025f72d2b7893
kevin4809 <kevinedk33@gmail.com>
2022-06-12 18:11:14 -0500

    1
    1

--- src/components/Navbarr.jsx
+++ src/components/Navbarr.jsx
@@ -17,7 +17,7 @@ const Navbarr = () => {
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">E-COMERS</a>
+                    <a className="navbar-brand" href="/">E-COMMERS</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
